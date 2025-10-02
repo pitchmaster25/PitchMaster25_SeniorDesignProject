8aebaa651d8c3003cf88e595f7f2c5b0f41c8415
pitchmaster25 <pitchmaster25@gmail.com>
2025-10-02 16:51:04 -0500

    Update main.py
    This is to update the main file from the previous repo. Mistakes were made and lessons were learned.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-# Hello GitHub! does this actually work?
+
+# Imports here
+
+from motor_control import *
+
+# --------------------- ENTRY POINT TO THE PITCH MASTER SCRIPT ----------------------
+
+# Strictly using console commands
+
+selected_waveform, operating_speed, ramp_time = command_speed()
+
+while True:
+    command = input("\nType the command you would like to execute: ")
+    match command:
+        case "help":
+            print('''Here are a list of the commands: 
+
+command = commands the motor how to operate            
+start = runs the motor start up sequence
+stop = runs the motor stop sequence
+exit = exits the program''')
+        case "command":
+            command_speed()
+        case "start":
+            start_sequence(operating_speed, ramp_time)
+        case "exit":
+            break
+
